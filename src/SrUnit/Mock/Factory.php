b73a4dbac9d808d1ad2318f64e328bdc9832b7a1
Jens Wiese <j.wiese@superreal.de>
2014-02-16 21:31:22 +0100

    Implement Factory to adapt Mockery Implement method to mock _parent classes

---
+++ src/SrUnit/Mock/Factory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SrUnit\Mock;
+
+use Mockery;
+
+/**
+ * Class Factory
+ *
+ * @link http://www.superReal.de
+ * @copyright (C) superReal GmbH | Agentur für Neue Kommunikation
+ * @package SrUnit\Mock
+ * @author Jens Wiese <j.wiese AT superreal.de>
+ */
+class Factory extends Mockery
+{
+
+    /**
+     * Mocks _parent-class of used for multiinheritance in Oxid
+     *
+     * @param string $className
+     * @return Mockery\MockInterface
+     */
+    public static function mockParentClass($className)
+    {
+        $parentClassName = $className . '_parent';
+        $mock = self::mock('overload:' . $parentClassName);
+
+        return $mock;
+    }
+
+
+
+} 
