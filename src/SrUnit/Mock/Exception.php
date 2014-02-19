e05a5732f0fc48d034bd3929b3a7c93204db6810
Thomas Oppelt <t.oppelt@superreal.de>
2014-02-19 21:22:37 +0100

    Minor: Add mock exception and registry class.

---
+++ src/SrUnit/Mock/Exception.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Class SrUnitMockException
+ *
+ * This Software is the property of superReal and is protected
+ * by copyright law - it is NOT Freeware.
+ *
+ * Any unauthorized use of this software without a valid license key
+ * is a violation of the license agreement and will be prosecuted by
+ * civil and criminal law.
+ *
+ * @link      http://www.superreal.de
+ * @package SrUnit\Mock
+ * @copyright (C) superReal 2014
+ * @author    Thomas Oppelt <t.oppelt _AT_ superreal.de>
+ */
+
+namespace SrUnit\Mock;
+
+
+class Exception extends \Exception
+{
+
+} 
---
+++ src/SrUnit/Mock/Registry.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Class Registry
+ *
+ * This Software is the property of superReal and is protected
+ * by copyright law - it is NOT Freeware.
+ *
+ * Any unauthorized use of this software without a valid license key
+ * is a violation of the license agreement and will be prosecuted by
+ * civil and criminal law.
+ *
+ * @link      http://www.superreal.de
+ * @package SrUnit\Mock
+ * @copyright (C) superReal 2014
+ * @author    Thomas Oppelt <t.oppelt _AT_ superreal.de>
+ */
+
+namespace SrUnit\Mock;
+
+
+class Registry
+{
+
+    /**
+     * Instance array
+     *
+     * @var array
+     */
+    protected static $instances = array();
+
+
+    /**
+     * Instance getter
+     *
+     * @param $className
+     * @return mixed
+     * @throws Exception
+     */
+    public static function get($className)
+    {
+        $className = strtolower($className);
+        if (isset(self::$instances[$className])) {
+            return self::$instances[$className];
+        } else {
+            throw new Exception("Mock instance missing for {$className}");
+        }
+    }
+
+    /**
+     * Instance setter
+     *
+     * @param string $className Class name
+     * @param object $instance Object instance
+     *
+     * @static
+     *
+     * @return null
+     */
+    public static function set($className, $instance)
+    {
+        $className = strtolower($className);
+
+        if (is_null($instance)) {
+            unset(self::$instances[$className]);
+            return;
+        }
+
+        self::$instances[$className] = $instance;
+    }
+
+    /**
+     * Get instance keys
+     *
+     * @return array
+     */
+    public static function getKeys()
+    {
+        return array_keys(self::$instances);
+    }
+
+} 
