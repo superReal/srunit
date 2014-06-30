2ac4e33816a183ccd2e591f396a62a70d47b0e98
Jens Wiese <j.wiese@superreal.de>
2014-06-30 12:27:52 +0200

    Minor: Add convinence method in TestCase to create virtual filesystem

--- src/SrUnit/TestCase.php
+++ src/SrUnit/TestCase.php
@@ -2,7 +2,10 @@
 
 namespace SrUnit;
 
+use org\bovigo\vfs\vfsStreamDirectory;
 use PHPUnit_Framework_TestCase;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamFile;
 
 /**
  * Class TestCase extends PHPUnit default testcase
@@ -14,6 +17,26 @@ use PHPUnit_Framework_TestCase;
  */
 class TestCase extends PHPUnit_Framework_TestCase
 {
+    /**
+     * Returns virtual filesystem for given root directory (based on vfsStream)
+     *
+     * @param string $rootDirName
+     * @param int $permissions
+     * @param array $structure
+     * @return vfsStreamDirectory
+     */
+    public function createVirtualFileSystem($rootDirName, $permissions = null, array $structure = array())
+    {
+        return vfsStream::setup($rootDirName, $permissions, $structure);
+    }
 
-
-} 
+    /**
+     * @param string $name
+     * @param int $permissions
+     * @return vfsStreamFile
+     */
+    public function createVirtualFile($name, $permissions = null)
+    {
+        return new vfsStreamFile($name, $permissions);
+    }
+}
